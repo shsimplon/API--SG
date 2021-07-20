a411c507b926ab7891b4f86e25b597d99eb17397
samira <samira@houacines-MacBook-Pro.local>
2021-07-20 20:03:11 +0200

    navlink

--- client/src/pages/Profil.js
+++ client/src/pages/Profil.js
@@ -1,7 +1,13 @@
-import React from 'react';
+/* eslint-disable react/jsx-no-comment-textnodes */
+// import React, { useContext } from 'react';
+// import { UidContext } from "../components/AppContext"
 import Log from '../components/Log';
+import RajouterRecette from './RajouterRecette';
+
 
 const Profil = () => {
+    // const uid=useContext(UidContext)
+
     return (
         <div className="profil-page">
             <div className="log-container">
@@ -11,6 +17,7 @@ const Profil = () => {
                    <img src="./img/img-connexion.jpg" alt=" ordinateur" />
                </div>
             </div>
+           
         </div>
     );
 };
