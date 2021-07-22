212f833626ca48e9fb64910f88fb5128b06ff4c9
samira <samira@houacines-MacBook-Pro.local>
2021-07-23 00:18:24 +0200

    modeles avancés

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,6 @@ require("express-async-errors");
 
 const restaurantsRouter= require("./restaurants_router");
 const patisseriesRouter=require("./patisseries_router")
-const astucesRouter=require("./astuces_router")
 const recettesRouter=require("./recettes_router")
 const userRouter = require ('./users_router.js')
 //  const uploadController = require ("./upload_router")
@@ -13,7 +12,6 @@ const mainRouter = express.Router();
 
 mainRouter.use("/restaurants",restaurantsRouter);
 mainRouter.use("/patisseries",patisseriesRouter);
-mainRouter.use("/astuces",astucesRouter);
 mainRouter.use("/recettes",recettesRouter);
 mainRouter.use('/users',userRouter);
 //unpload
