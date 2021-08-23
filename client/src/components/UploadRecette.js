76d3e66aec74710a60c7deb23fb44732efb2fd61
samira <samira@houacines-MacBook-Pro.local>
2021-08-23 15:49:12 +0200

    update recette

--- client/src/components/UploadRecette.js
+++ client/src/components/UploadRecette.js
@@ -1,122 +1,4 @@
-// /* eslint-disable no-dupe-keys */
-// /* eslint-disable no-unused-expressions */
-// /* eslint-disable no-unused-vars */
-// /* eslint-disable no-restricted-globals */
-// /* eslint-disable no-undef */
-// import axios from "axios";
-// import React, { useState, useEffect } from "react";
-// import api from "../services/AuthApi";
-// import { useForm } from "react-hook-form";
-
-// const UploadRecette = (props) => {
-//   //   const [data, setData] = useState([]);
-//   const [avatar, setAvatar] = useState("");
-//   const [name, setName] = useState();
-//   const [ingredients, setIngredients] = useState();
-//   const [preparations, setPreparations] = useState();
-
-//   const {
-//     register,
-//     handleSubmit,
-//     formState: { errors },
-//   } = useForm();
-
-//   const onSubmit = (data) => async (event, recette) => {
-//     event.preventDefault();
-//     const token = localStorage.getItem("jwt");
-
-//     const imgData = new FormData();
-//     imgData.append("name", name);
-//     imgData.append("avatar", avatar);
-//     imgData.append("ingredients", ingredients);
-//     imgData.append("preparations", preparations);
-//     const response = await api.post(`/api/recettes/upload`, imgData);
-//     props.setData([...props.data, response.data]);
-//   };
-
-//   return (
-//     <div>
-//       <form
-//         action="/uploadfile"
-//         enctype="multipart/form-data"
-//         className="update-container"
-//         onSubmit={handleSubmit(onSubmit)}
-//       >
-//         <div className="update-recette form-recette">
-//           <textarea
-//             {...register("name", { required: true })}
-//             className="textarea-form"
-//             name="name"
-//             type="text"
-//             rows="2"
-//             placeholder="Nom de la recette"
-//             onChange={(event) => {
-//               const { value } = event.target;
-//               setName(value);
-//             }}
-//           />
-//           {errors.name && <p>Last name is required.</p>}
-//           <br />
-//           <textarea
-//             {...register("ingredients", { required: true })}
-//             className="textarea-form"
-//             name="ingredients"
-//             type="text"
-//             placeholder="Ingredients"
-//             rows="15"
-//             onChange={(event) => {
-//               const { value } = event.target;
-//               setIngredients(value);
-//             }}
-//           />
-//           {errors.name && <p>Last ingredients is required.</p>}
-//           <br />
-//           <textarea
-//             className="textarea-form"
-//             {...register("preparations", { required: true })}
-//             name="preparations"
-//             type="text"
-//             placeholder="Preparations"
-//             rows="15"
-//             onChange={(event) => {
-//               const { value } = event.target;
-//               setPreparations(value);
-//             }}
-//           />
-//           {errors.name && <p>Last preparations is required.</p>}
-//           <br />
-//           <input type="submit" value="+ Ajouter recette" />
-//           {/* <button onClick={onSubmit} type="submit"> */}{" "}
-//           {/* + Ajouter recette
-//           </button>{" "} */}
-//         </div>
-//         <div className="profil-page">
-//           <div className="left-part">
-//             <input
-//               //   {...register("file")}
-//               type="file"
-//               id="file-upload"
-//               name="file"
-//               accept="image/*"
-//               onChange={(e) => setAvatar(e.target.files[0])}
-//             />
-//             {errors.name && <p>Last name is required.</p>}
-//           </div>
-//         </div>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default UploadRecette;
-
-/* eslint-disable no-dupe-keys */
-/* eslint-disable no-unused-expressions */
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-restricted-globals */
-/* eslint-disable no-undef */
-import axios from "axios";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import api from "../services/AuthApi";
 
 const UploadRecette = (props) => {
@@ -125,23 +7,39 @@ const UploadRecette = (props) => {
   const [name, setName] = useState();
   const [ingredients, setIngredients] = useState();
   const [preparations, setPreparations] = useState();
+  const [message, setMessage] = useState();
 
   const onSubmit = async (event, recette) => {
     event.preventDefault();
     const token = localStorage.getItem("jwt");
 
-    const imgData = new FormData();
-    imgData.append("name", name);
-    imgData.append("avatar", avatar);
-    imgData.append("ingredients", ingredients);
-    imgData.append("preparations", preparations);
+    if (name == null || name == "") {
+      setMessage(" le name est obligatoire");
+    } else if (ingredients == null || ingredients == "") {
+      setMessage(" les ingrédients sont obligatoires");
+    } else if (preparations == null || preparations == "") {
+      setMessage(" les préparations sont obligatoires");
+    }
+    if (!message) {
+      const imgData = new FormData();
+      imgData.append("name", name);
+      imgData.append("avatar", avatar);
+      imgData.append("ingredients", ingredients);
+      imgData.append("preparations", preparations);
 
-    const response = await api.post(`/api/recettes/upload`, imgData);
-    props.setData([...props.data, response.data]);
+      const response = await api.post(`/api/recettes/upload`, imgData);
+      props.setData([...props.data, response.data]);
+    }
   };
 
   return (
     <div>
+      {message && (
+        <p style={{ color: " rgb(220,52,68)", paddingLeft: "40px" }}>
+          {message}
+        </p>
+      )}
+
       <form
         action="/uploadfile"
         enctype="multipart/form-data"
