9ebb38b5e397d6d6edec6772941a8debf93e43e9
samira <samira@houacines-MacBook-Pro.local>
2021-11-01 12:30:17 +0100

    api

--- client/src/pages/Profil.js
+++ client/src/pages/Profil.js
@@ -1,17 +1,21 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
-// import { UidContext } from "../components/AppContext"
 import Log from "../components/Log";
+import Footer from "../components/Footer/Footer";
 
 const Profil = () => {
   return (
-    <div className="profil-page">
-      <div className="log-container">
-        <Log signin={false} signup={true} />
-        <div className="img-container">
-          <img src="./img/img-connexion.jpg" alt=" ordinateur" />
+    <>
+      <div className="profil-page">
+        <div className="log-container">
+          <Log signin={false} signup={true} />
+          <div className="img-container">
+            <img src="./img/img-connexion.jpg" alt=" ordinateur" />
+          </div>
         </div>
       </div>
-    </div>
+      <br />
+      <Footer />
+    </>
   );
 };
 
